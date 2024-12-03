6ca18024d75e30d7619bb208af8e977aa381f3a6
Michael Dilts <michael.curtis.dilts@gmail.com>
2024-12-03 14:52:27 -0800

    not sure why im not getting values here...

--- day3/day3.py
+++ day3/day3.py
@@ -1,8 +1,13 @@
 import re
 #variables
 pattern = "mul\(\d{1,3},\d{1,3}\)"
+dopattern = "do\((.*?)don't\)"
 matches = []
 entries = []
+domatches = []
+doentries = []
+dontentries = []
+lastend = 0
 
 with open('day3\day3_input.txt', 'r') as file:
     plaintext = file.read()
@@ -14,8 +19,25 @@ for match in matches:
     result = values[0] * values[1]
     entries.append(result)
 
-print(matches)
-print(entries)
+domatches = list(re.finditer(dopattern, plaintext, re.DOTALL))
+
+for match in domatches:
+
+    validmatches = match.group(1).strip()
+
+    doentryvalues = re.findall(pattern, validmatches)
+
+    for value in doentryvalues:
+        values = tuple(map(int, re.findall(r"\d+", value)))
+        result = values[0] * values[1]
+        doentries.append(result)
+
+if lastend < len(plaintext):
+    dontentries.append(plaintext[lastend:].strip())
+
+print(doentries)
+print(sum(doentries))
 print(sum(entries))
+
 
 
